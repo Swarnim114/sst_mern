f826b6f360e09e14a0ab0bee92dbc133f2754df5
Swarnim114 <hofboy04@gmail.com>
2024-07-12 10:06:18 +0530

    axios setup

---
+++ client/src/calls/users.js
@@ -0,0 +1,12 @@
+const {axiosInstance} = require('./index');
+
+//register new user
+
+export const RegisterUser = async (value) =>{
+    try{
+        const response = await axiosInstance.post("api/users/register" , value);
+        return response.data;
+    }catch(error){
+        console.log(error);
+    }
+}
